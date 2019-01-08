7a9088ef9f37ff7280818340c562ede70f2a1e5e
FatmaSharkawy <fdaify@iskyev.com>
2019-01-08 23:26:26 +0200

    [UPD] excepcted date in sale order

--- bi_sale_customization/models/sale_order_inherit.py
+++ bi_sale_customization/models/sale_order_inherit.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
 from odoo import models, fields, api
+from datetime import datetime, timedelta
 
 
 class SaleOrder(models.Model):
@@ -9,6 +9,21 @@ class SaleOrder(models.Model):
     po_number = fields.Char(string="PO Number")
     shipment_number = fields.Char(string="Shipment Number")
     print_hs_code = fields.Boolean(string="Print HS Code")
+    expected_date = fields.Datetime("Expected Date", compute=False, readonly=False, store=True,
+                                    help="Delivery date you can promise to the customer, computed from product lead times and from the shipping policy of the order.")
+
+    @api.multi
+    @api.onchange('order_line.customer_lead', 'confirmation_date', 'order_line')
+    def change_expected_date(self):
+        for order in self:
+            dates_list = []
+            confirm_date = fields.Datetime.from_string(
+                (order.confirmation_date or order.write_date) if order.state == 'sale' else fields.Datetime.now())
+            for line in order.order_line.filtered(lambda x: x.state != 'cancel' and not x._is_delivery()):
+                dt = confirm_date + timedelta(days=line.customer_lead or 0.0)
+                dates_list.append(dt)
+            if dates_list:
+                order.expected_date = fields.Datetime.to_string(min(dates_list))
 
     @api.multi
     def _prepare_invoice(self):
