db6d8055ad4e9699e5dc146eafe3c52a272ae3b7
FatmaSharkawy <fdaify@iskyev.com>
2019-01-11 02:51:38 +0200

    [ADD] lot reservation module

---
+++ bi_lot_reservation/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'BI Lot Reservation',
+    'summary': 'BI Lot Reservation',
+    'description': """
+
+     """,
+    'author': "BI Solutions Development Team",
+    'category': 'Warehouse',
+    'version': '0.1',
+    'depends': ['stock', ],
+    'data': [
+        'views/stock_move_inherit_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'sequence': 1
+}
---
+++ bi_lot_reservation/models/stock_move_inherit.py
@@ -0,0 +1,2 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
