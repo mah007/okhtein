026b7ef0be480f404e2456d806a2ab93e1f8354b
FatmaSharkawy <fdaify@iskyev.com>
2019-01-14 23:57:53 +0200

    [DELETE] pos retail module

--- pos_retail/reports/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import pos_sale_analytic
-from . import pos_manufacturing_report
-from . import pos_tracking_client
-from . import pos_order_report
--- pos_retail/wizards/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import stock_immediate_transfer
-from . import account_invoice_refund
-from . import sale_order_line_insert
-from . import remove_pos_order
-from . import cashbox
