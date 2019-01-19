accc75e34322e7b4fe78872ec3bcfdace17081c2
FatmaSharkawy <fdaify@iskyev.com>
2019-01-20 01:20:14 +0200

    [UPD] repair module

--- bi_repair_customization/models/repair_order_inherit.py
+++ bi_repair_customization/models/repair_order_inherit.py
@@ -7,25 +7,24 @@ from odoo.tools import float_compare
 class RepairOrder(models.Model):
     _inherit = 'repair.order'
 
-    receive_date = fields.Date(string="Received Date")
-    finish_date = fields.Date(string="Finished Date")
-    location_id = fields.Many2one(
-        'stock.location', 'Delivered Location',
-        index=True, readonly=True, required=True,
-        help="This is the location where the product to repair is located.",
-        states={'draft': [('readonly', False)], 'confirmed': [('readonly', True)]})
-    location_destination = fields.Many2one(
-        'stock.location', 'Destination Location',
-        index=True, required=True)
-    source_location = fields.Many2one(
-        'stock.location', 'Source Location (Receipt)',
-        index=True, required=True)
-    destination_location = fields.Many2one(
-        'stock.location', 'Destination Location (Receipt)',
-        index=True, required=True)
-    confirm_id = fields.Many2one(
-        'stock.move', 'To confirm',
-        copy=False, readonly=True)
+    receive_date = fields.Date(string="Received Date", )
+    finish_date = fields.Date(string="Finished Date", )
+    location_id = fields.Many2one('stock.location', string='Delivered Location', required=True, index=False,
+                                  default=False,
+                                  states={'draft': [('readonly', False)], 'confirmed': [('readonly', True)]})
+
+    destination_location_id = fields.Many2one('stock.location', string='Destination Location', required=True,
+                                              default=False, states={'draft': [('readonly', False)],
+                                                                     'confirmed': [('readonly', True)]})
+    receipt_source_location_id = fields.Many2one('stock.location', string='Source Location (Receipt)', required=True,
+                                                 default=False, states={'draft': [('readonly', False)],
+                                                                        'confirmed': [('readonly', True)]})
+    receipt_destination_location_id = fields.Many2one('stock.location', string='Destination Location (Receipt)',
+                                                      required=True, default=False,
+                                                      states={'draft': [('readonly', False)],
+                                                              'confirmed': [('readonly', True)]})
+    receipt_stock_picking_id = fields.Many2one('stock.picking', 'Receipt', copy=False, readonly=True)
+    delivery_stock_picking_id = fields.Many2one('stock.picking', 'Delivery', copy=False, readonly=True)
 
     @api.constrains('receive_date', 'finish_date')
     def check_repair_order_dates(self):
@@ -34,162 +33,82 @@ class RepairOrder(models.Model):
                 if order.receive_date > order.finish_date:
                     raise ValidationError(_('Order received date must be less than order finished date.'))
 
+    @api.onchange('receipt_source_location_id')
+    def set_destination_location(self):
+        for order in self:
+            if order.receipt_source_location_id:
+                order.destination_location_id = order.receipt_source_location_id.id
+
+    @api.onchange('receipt_destination_location_id')
+    def set_location(self):
+        for order in self:
+            if order.receipt_destination_location_id:
+                order.location_id = order.receipt_destination_location_id.id
+
+    @api.multi
+    def action_validate(self):
+        return self.action_repair_confirm()
+
     @api.multi
     def action_repair_confirm(self):
-        if self.filtered(lambda repair: repair.state != 'draft'):
-            raise UserError(_("Repair must be under repair in order to end reparation."))
-        for repair in self:
-            repair.write({'repaired': True})
-            vals = {'state': 'draft'}
-            vals['confirm_id'] = repair.action_confirm_done().get(repair.id)
-            if not repair.invoiced and repair.invoice_method == 'after_repair':
-                vals['state'] = '2binvoiced'
-            repair.write(vals)
+        stock_picking_type_id = self.env['stock.picking.type'].search(
+            [('code', '=', 'incoming')], limit=1).id
+        for order in self:
+            if not stock_picking_type_id:
+                raise ValidationError(_('Please configure receipt picking type for your company.'))
+            picking_vals = {
+                'origin': order.name,
+                'partner_id': order.partner_id.id,
+                'scheduled_date': fields.Datetime.now(),
+                'picking_type_id': stock_picking_type_id,
+                'location_id': order.receipt_source_location_id.id,
+                'location_dest_id': order.receipt_destination_location_id.id,
+                'company_id': order.company_id.id,
+                'move_type': 'direct',
+                'state': 'waiting',
+            }
+            created_picking_object = self.env['stock.picking'].create(picking_vals)
+            created_move_object = self.env['stock.move'].create({
+                'product_id': order.product_id.id,
+                'name': order.product_id.name,
+                'product_uom_qty': order.product_qty,
+                'product_uom': order.product_uom.id,
+                'company_id': order.company_id.id,
+                'location_id': order.receipt_source_location_id.id,
+                'location_dest_id': order.receipt_destination_location_id.id,
+                'picking_id': created_picking_object.id,
+            })
 
-        before_repair = self.filtered(lambda repair: repair.invoice_method == 'b4repair')
-        before_repair.write({'state': '2binvoiced'})
-        to_confirm = self - before_repair
-        to_confirm_operations = to_confirm.mapped('operations')
-        to_confirm_operations.write({'state': 'confirmed'})
-        to_confirm.write({'state': 'confirmed'})
-        return True
+            order.write({'state': 'confirmed', 'receipt_stock_picking_id': created_picking_object.id})
 
     @api.multi
-    def action_confirm_done(self):
-        res = {}
-        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
-        Move = self.env['stock.move']
-        for repair in self:
-            # Try to create move with the appropriate owner
-            owner_id = False
-            available_qty_owner = self.env['stock.quant']._get_available_quantity(repair.product_id, repair.location_id,
-                                                                                  repair.lot_id,
-                                                                                  owner_id=repair.partner_id,
-                                                                                  strict=True)
-            if float_compare(available_qty_owner, repair.product_qty, precision_digits=precision) >= 0:
-                owner_id = repair.partner_id.id
-
-            moves = self.env['stock.move']
-            for operation in repair.operations:
-                move = Move.create({
-                    'name': repair.name,
-                    'product_id': operation.product_id.id,
-                    'product_uom_qty': operation.product_uom_qty,
-                    'product_uom': operation.product_uom.id,
-                    'partner_id': repair.address_id.id,
-                    'location_id': operation.location_id.id,
-                    'location_dest_id': operation.location_dest_id.id,
-                    'move_line_ids': [(0, 0, {'product_id': operation.product_id.id,
-                                              'lot_id': operation.lot_id.id,
-                                              'product_uom_qty': 0,  # bypass reservation here
-                                              'product_uom_id': operation.product_uom.id,
-                                              'qty_done': operation.product_uom_qty,
-                                              'package_id': False,
-                                              'result_package_id': False,
-                                              'owner_id': owner_id,
-                                              'location_id': operation.location_id.id,  # TODO: owner stuff
-                                              'location_dest_id': operation.location_dest_id.id, })],
-                    'repair_id': repair.id,
-                    'origin': repair.name,
-                })
-                moves |= move
-                operation.write({'confirm_id': move.id, 'state': 'draft'})
-            move = Move.create({
-                'name': repair.name,
-                'product_id': repair.product_id.id,
-                'product_uom': repair.product_uom.id or repair.product_id.uom_id.id,
-                'product_uom_qty': repair.product_qty,
-                'partner_id': repair.address_id.id,
-                'location_id': repair.source_location.id,
-                'location_dest_id': repair.destination_location.id,
-                'move_line_ids': [(0, 0, {'product_id': repair.product_id.id,
-                                          'lot_id': repair.lot_id.id,
-                                          'product_uom_qty': 0,  # bypass reservation here
-                                          'product_uom_id': repair.product_uom.id or repair.product_id.uom_id.id,
-                                          'qty_done': repair.product_qty,
-                                          'package_id': False,
-                                          'result_package_id': False,
-                                          'owner_id': owner_id,
-                                          'location_id': repair.source_location.id,  # TODO: owner stuff
-                                          'location_dest_id': repair.destination_location.id, })],
-                'repair_id': repair.id,
-                'origin': repair.name,
+    def action_repair_end(self):
+        stock_picking_type_id = self.env['stock.picking.type'].search(
+            [('code', '=', 'outgoing')], limit=1).id
+        for order in self:
+            if not stock_picking_type_id:
+                raise ValidationError(_('Please configure delivery picking type for your company.'))
+            picking_vals = {
+                'origin': order.name,
+                'partner_id': order.partner_id.id,
+                'scheduled_date': fields.Datetime.now(),
+                'picking_type_id': stock_picking_type_id,
+                'location_id': order.location_id.id,
+                'location_dest_id': order.destination_location_id.id,
+                'company_id': order.company_id.id,
+                'move_type': 'direct',
+                'state': 'waiting',
+            }
+            created_picking_object = self.env['stock.picking'].create(picking_vals)
+            created_move_object = self.env['stock.move'].create({
+                'product_id': order.product_id.id,
+                'name': order.product_id.name,
+                'product_uom_qty': order.product_qty,
+                'product_uom': order.product_uom.id,
+                'company_id': order.company_id.id,
+                'location_id': order.location_id.id,
+                'location_dest_id': order.destination_location_id.id,
+                'picking_id': created_picking_object.id,
             })
-            consumed_lines = moves.mapped('move_line_ids')
-            produced_lines = move.move_line_ids
-            moves |= move
-            moves._action_done()
-            produced_lines.write({'consume_line_ids': [(6, 0, consumed_lines.ids)]})
-            res[repair.id] = move.id
-        return res
 
-    @api.multi
-    def action_repair_done(self):
-        if self.filtered(lambda repair: not repair.repaired):
-            raise UserError(_("Repair must be repaired in order to make the product moves."))
-        res = {}
-        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
-        Move = self.env['stock.move']
-        for repair in self:
-            # Try to create move with the appropriate owner
-            owner_id = False
-            available_qty_owner = self.env['stock.quant']._get_available_quantity(repair.product_id, repair.location_id,
-                                                                                  repair.lot_id,
-                                                                                  owner_id=repair.partner_id,
-                                                                                  strict=True)
-            if float_compare(available_qty_owner, repair.product_qty, precision_digits=precision) >= 0:
-                owner_id = repair.partner_id.id
-
-            moves = self.env['stock.move']
-            for operation in repair.operations:
-                move = Move.create({
-                    'name': repair.name,
-                    'product_id': operation.product_id.id,
-                    'product_uom_qty': operation.product_uom_qty,
-                    'product_uom': operation.product_uom.id,
-                    'partner_id': repair.address_id.id,
-                    'location_id': operation.location_id.id,
-                    'location_dest_id': operation.location_dest_id.id,
-                    'move_line_ids': [(0, 0, {'product_id': operation.product_id.id,
-                                              'lot_id': operation.lot_id.id,
-                                              'product_uom_qty': 0,  # bypass reservation here
-                                              'product_uom_id': operation.product_uom.id,
-                                              'qty_done': operation.product_uom_qty,
-                                              'package_id': False,
-                                              'result_package_id': False,
-                                              'owner_id': owner_id,
-                                              'location_id': operation.location_id.id,  # TODO: owner stuff
-                                              'location_dest_id': operation.location_dest_id.id, })],
-                    'repair_id': repair.id,
-                    'origin': repair.name,
-                })
-                moves |= move
-                operation.write({'move_id': move.id, 'state': 'done'})
-            move = Move.create({
-                'name': repair.name,
-                'product_id': repair.product_id.id,
-                'product_uom': repair.product_uom.id or repair.product_id.uom_id.id,
-                'product_uom_qty': repair.product_qty,
-                'partner_id': repair.address_id.id,
-                'location_id': repair.location_id.id,
-                'location_dest_id': repair.location_destination.id,
-                'move_line_ids': [(0, 0, {'product_id': repair.product_id.id,
-                                          'lot_id': repair.lot_id.id,
-                                          'product_uom_qty': 0,  # bypass reservation here
-                                          'product_uom_id': repair.product_uom.id or repair.product_id.uom_id.id,
-                                          'qty_done': repair.product_qty,
-                                          'package_id': False,
-                                          'result_package_id': False,
-                                          'owner_id': owner_id,
-                                          'location_id': repair.location_id.id,  # TODO: owner stuff
-                                          'location_dest_id': repair.location_destination.id, })],
-                'repair_id': repair.id,
-                'origin': repair.name,
-            })
-            consumed_lines = moves.mapped('move_line_ids')
-            produced_lines = move.move_line_ids
-            moves |= move
-            moves._action_done()
-            produced_lines.write({'consume_line_ids': [(6, 0, consumed_lines.ids)]})
-            res[repair.id] = move.id
-        return res
+            order.write({'state': 'done', 'delivery_stock_picking_id': created_picking_object.id})
